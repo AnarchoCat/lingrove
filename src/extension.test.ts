0aee860cbccc7c707d0ee5f78dccfb5c546379e0
Anarcho Cat <d2717p@gmail.com>
2025-01-29 13:08:34 +0900

    chore: remove redundant codes

--- src/extension.test.ts
+++ src/extension.test.ts
@@ -1,17 +1,8 @@
 import * as assert from 'assert'
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode'
-import './another.test'
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.')
-
-	test('Another test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5))
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0))
-	})
 
 	test('Check if extension is active', async () => {
 		const extension = vscode.extensions.getExtension('AnarchoCat.mmimy')
