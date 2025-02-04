0d0fe5c46a013327d003a2f2adcc541341973cde
Anarcho Cat <d2717p@gmail.com>
2025-02-04 16:49:43 +0900

    feat: add labels to languages

--- src/commands/selectLanguage.ts
+++ src/commands/selectLanguage.ts
@@ -3,19 +3,26 @@ import * as vscode from 'vscode'
 export async function selectLanguage() {
 	const config = vscode.workspace.getConfiguration('mmimy')
 	// Define the options
-	const options = ['en', 'vi', 'es']
+	const options: Record<string, string> = {
+		English: 'en',
+		Vietnamese: 'vi',
+		Spanish: 'es',
+	}
 	// Show the quick pick list
-	const selectedOption = await vscode.window.showQuickPick(options, {
-		placeHolder: 'Select a language',
-		canPickMany: false,
-		ignoreFocusOut: true,
-	})
+	const selectedOption = await vscode.window.showQuickPick(
+		Object.keys(options),
+		{
+			placeHolder: 'Select a language',
+			canPickMany: false,
+			ignoreFocusOut: true,
+		},
+	)
 
 	if (selectedOption) {
 		// Update the setting with the selected option
 		await config.update(
 			'language',
-			selectedOption,
+			options[selectedOption],
 			vscode.ConfigurationTarget.Global,
 		)
 		vscode.window.showInformationMessage(`Language set to: ${selectedOption}`)
