21dd385b37524c3512273d8f8044bda24231d0e0
Anarcho Cat <d2717p@gmail.com>
2025-02-05 16:18:17 +0900

    feat: improve select language

--- src/commands/selectLanguage.ts
+++ src/commands/selectLanguage.ts
@@ -1,24 +1,34 @@
 import Mmimy from '@/mmimy'
 import * as vscode from 'vscode'
+import languages from '@assets/languages.json'
+
+interface Option extends vscode.QuickPickItem {
+	value: string
+}
+
 export default async function selectLanguage() {
-	// Define the options
-	const options: Record<string, string> = {
-		English: 'en',
-		Vietnamese: 'vi',
-		Spanish: 'es',
+	const options: Option[] = []
+	const values = languages.enum
+	const labels = languages.enumItemLabels
+	const descriptions = languages.enumDescriptions
+	for (let i = 0; i < values.length; i++) {
+		options.push({
+			label: labels[i],
+			value: values[i],
+			description: descriptions[i],
+		})
 	}
 	// Show the quick pick list
-	const selectedOption = await vscode.window.showQuickPick(
-		Object.keys(options),
-		{
-			placeHolder: 'Select a language',
-			canPickMany: false,
-			ignoreFocusOut: true,
-		},
-	)
+	const selectedOption = await vscode.window.showQuickPick(options, {
+		placeHolder: 'Select a language',
+		canPickMany: false,
+		ignoreFocusOut: true,
+	})
 	if (selectedOption) {
-		vscode.window.showInformationMessage(`Language set to: ${selectedOption}`)
-		Mmimy.getInstance().language = options[selectedOption]
+		vscode.window.showInformationMessage(
+			`Language set to: ${selectedOption.label}`,
+		)
+		Mmimy.getInstance().language = selectedOption.value
 	} else {
 		vscode.window.showInformationMessage('No language selected.')
 	}
