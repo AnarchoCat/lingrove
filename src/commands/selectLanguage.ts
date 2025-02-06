064fbda61e99001fd5449c7cd0a0ec7ffbc3a384
Anarcho Cat <d2717p@gmail.com>
2025-02-06 12:35:19 +0900

    feat: allow user to add additional languages

--- src/commands/selectLanguage.ts
+++ src/commands/selectLanguage.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode'
 import languages from '@assets/languages.json'
 
 interface Option extends vscode.QuickPickItem {
-	value: string
+	code: string
 }
 
 export default async function selectLanguage() {
@@ -14,21 +14,27 @@ export default async function selectLanguage() {
 	for (let i = 0; i < values.length; i++) {
 		options.push({
 			label: labels[i],
-			value: values[i],
+			code: values[i],
 			description: descriptions[i],
 		})
 	}
+	const additionalLanguages =
+		vscode.workspace
+			.getConfiguration('mmimy')
+			.get<Option[]>('additionalLanguages') ?? []
 	// Show the quick pick list
-	const selectedOption = await vscode.window.showQuickPick(options, {
-		placeHolder: 'Select a language',
-		canPickMany: false,
-		ignoreFocusOut: true,
-	})
+	const selectedOption = await vscode.window.showQuickPick(
+		options.concat(additionalLanguages),
+		{
+			placeHolder: 'Select a language',
+			canPickMany: false,
+		},
+	)
 	if (selectedOption) {
 		vscode.window.showInformationMessage(
 			`Language set to: ${selectedOption.label}`,
 		)
-		Mmimy.getInstance().language = selectedOption.value
+		Mmimy.getInstance().language = selectedOption.code
 	} else {
 		vscode.window.showInformationMessage('No language selected.')
 	}
